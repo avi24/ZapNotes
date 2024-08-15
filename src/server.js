006f2d63e8c6110f71362b597479119f6c3cc8c6
avi24 <2397222+avi24@users.noreply.github.com>
2024-08-15 01:15:10 -0700

    chore: added dependencies and updated static and src code
    - added Express dependency and subsequent package-lock
    - updated src: server.js to use Express logic
    - renamed static: css style to styles

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
+/*
 const http = require('http');
+
 const PORT = 9000;
 
 const server = http.createServer((req, res) => {
@@ -9,4 +11,40 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
+});
+*/
+
+//Import modules
+const express = require('express');
+const path = require('path');
+
+// Create an Express app
+const app = express();
+
+// Define PORT
+// TODO: Define port in .env later on
+const PORT = 9000;
+
+// Middleware - Handle JSON requests 
+app.use(express.json());
+// Middleware - Serve static pages from 'static' directory
+app.use(express.static(path.join(__dirname, '../static')));
+
+// Route for homepage
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Define an API route for notes
+app.get('/api/notes', (req, res) => {
+    res.json({ message: 'Hello from the API!' });
+});
+// Define an API route for users
+app.get('/api/users', (req, res) => {
+    res.json({ message: 'Hello from the API!' });
+});
+  
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
