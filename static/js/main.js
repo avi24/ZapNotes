652abb39ab5737eda0af930b10b7b2a97b74e645
avi24 <2397222+avi24@users.noreply.github.com>
2024-08-18 00:35:23 -0700

    static: connected front-end to MongoDB, added functionalty to display data on homepage

--- static/js/main.js
+++ static/js/main.js
@@ -7,23 +7,34 @@ const userInfo = document.querySelector("#userInfo")
 // Add event listener to the fetch button
 fetchButton.addEventListener("click", () => {
     // Set endpoint url
-    const url = "http://localhost:9000/api/users/";
+    const url = "http://localhost:9000/notes/";
 
     // Fetch data from endpoint
     fetch(url)
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             // Clear the userInfo panel
             userInfo.innerHTML = '';
 
-            const userElement = document.createElement("div");
+            // Loop through the data and append to the userInfo panel
+            data.forEach(note => {
+                const noteElement = document.createElement("div");
+                noteElement.innerHTML = `
+                <h2>${note.title}</h2>
+                <p>${note.message}</p>
+                `
+                userInfo.appendChild(noteElement);
+            })
 
-            userElement.innerHTML = `
-            <h2>Name: ${data.name.title} ${data.name.first} ${data.name.last}</h2>
-            <p>Email: ${data.email}</p>
-            <p>Age: ${data.dob.age}</p>
-            `;
-            userInfo.appendChild(userElement);
+            // const userElement = document.createElement("div");
+
+            // userElement.innerHTML = `
+            // <h2>Name: ${data.name.title} ${data.name.first} ${data.name.last}</h2>
+            // <p>Email: ${data.email}</p>
+            // <p>Age: ${data.dob.age}</p>
+            // `;
+            // userInfo.appendChild(userElement);
         })
         .catch(error => {
             console.log(`Error: ${error.message}`);
