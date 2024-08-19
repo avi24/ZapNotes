87f426c0af179fbda419fe58b319f40c2ec275f0
avi24 <2397222+avi24@users.noreply.github.com>
2024-08-18 23:44:59 -0700

    major: added auth support to the app
    - added passport, passport-local, express-session dependencies
    - implemented prelim auth to the app

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 // const axios = require('axios');
 const connectDB = require('./config/db.js');
+const session = require('express-session');
+const passport = require('./auth/passport.js');
 const errorHandler = require('./middleware/errorHandler.js');
 
 // Import routes
@@ -31,14 +33,47 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Static file - Serve static pages from 'static' directory (ie. HTML, CSS)
 app.use(express.static(path.join(__dirname, '../static')));
-// Auth - Authenticate users making requests
-// TODO: implement auth middleware
+// Auth - Authenticate users
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+    if(!req.isAuthenticated) {
+        res.redirect('/login');
+        res.send("<h1>Home</h1><a href='/login'>Login</a>");
+    } else {
+        res.send(`<h1>Welcome ${req.user.username}</h1><a href='/logout'>Logout</a>`);
+    }
+});
+
+app.get('/login', (req, res) => {
+    res.send(__dirname + "/login.html");
+})
+
+app.post('/login', passport.authenticate('local',
+    {
+        successRedirect: '/',
+        failureRedirect: '/login',
+    }
+));
+
+app.get('/logout', (req,res) => {
+    req.logout((err) => {
+        if(err) { throw err; }
+        res.redirect('/login');
+    })
+});
 
 // Routes Handlers
 // Route for homepage
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+// });
 // app.use('/', homeRoute);
 // app.use('/notes', noteRoutes);
 app.use('/users', userRoutes);
---
+++ src/auth/passport.js
@@ -0,0 +1,36 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/userSchema.js');
+
+passport.use(new LocalStrategy(
+    async (username, password, done) => {
+        try {
+        const user = await User.findOne({ username });
+
+        // Validate user and password data
+        // TODO: implement verifyPassword method in model
+        if (!user || !user.verifyPassword(password)) {
+            return done(null, false, { message: 'Incorrect credentials.' });
+        }
+        
+            return done(null, user);
+        } catch (error) {
+            return done(error);
+        }
+    }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+});
+
+passport.deserializeUser(async (userId, done) => {
+    try {
+        const user = await User.findById(userId);
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});  
+
+module.exports = passport;
